3e8d9fca325f7d39fcca8fa596c76222d3c0dab4
OFFICIALHACKERERA <102041863+OFFICIALHACKERERA@users.noreply.github.com>
2022-07-11 17:52:31 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -51,7 +51,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🔰 Owner :- ✨ [Dhruba](https://t.me/DhrubaXD) ❤️**\n**Repo ~** [Repo](https://github.com/LEGENDARY-OS/MUSIC-BOT)"
+        rep = "**🔰 Owner :- ✨ [Dhruba](https://t.me/OFFICIALHACKERERA) ❤️**\n**Repo ~** [Repo](https://github.com/LEGENDARY-OS/MUSIC-BOT)"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
