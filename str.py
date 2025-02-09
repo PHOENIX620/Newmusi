8010b198498ca27c4cffce1cfab8d0c89f7bfe4e
PHOENIX620 <111209953+PHOENIX620@users.noreply.github.com>
2025-02-10 01:18:06 +0530

    Update str.py

--- str.py
+++ str.py
@@ -6,10 +6,11 @@ print("Enter your app information from my.telegram.org/apps below.")
 async def main():
     api_id = 14964504  # Your API ID
     api_hash = "7ddc2e405de3b79daa752d971f3336b8"  # Your API Hash
+    session_string = "BQDkVxgAVByicTqSuqnkTwrIFXhJLvMJ2pGwpPBP41HKkZuctVup5H-qR-IKxTxmT8jzIlhBYn4WWM_ZdBmroWAKBXxGSxstT5aTqupiN4KbYPj389PgMml4jSdosEq-HKM5j7_1azHDzhIYqnGiwX33FMGOQIRTIcLRwfa5NzFF7OqNZeZKLrGcDvbVX4iu62AezsHt3jATF6mVCs5Me2Yscr5a9auRQJ26rZM__NyeSte82BwywUxD0KndBE_sHNZeTn-w3csdaKO5rhC61A5CaDe00zgbFHjha74w2atXi0jrSu7DrUJdz37N0NibLdG8W3bncfY-ekEVZjuV52dJwrc2_wAAAAHOQYzfAA"  # Your session string
 
-    # Create a temporary client session and export the session string
-    async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
-        print(await app.export_session_string())  # This will print the session string
+    # Create a client session with the provided session string
+    async with Client(session_string, api_id=api_id, api_hash=api_hash) as app:
+        print("Session started successfully!")
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
