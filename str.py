0ffd7732fd18d21066c57fefd329d781d9fce352
PHOENIX620 <111209953+PHOENIX620@users.noreply.github.com>
2025-02-10 01:09:22 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,17 +1,15 @@
-
-
 import asyncio
-
 from pyrogram import Client
-
 
 print("Enter your app information from my.telegram.org/apps below.")
 
-
 async def main():
-    async with Client(":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")) as app:
-        print(await app.export_session_string())
+    api_id = 14964504  # Your API ID
+    api_hash = "7ddc2e405de3b79daa752d971f3336b8"  # Your API Hash
 
+    # Create a temporary client session and export the session string
+    async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
+        print(await app.export_session_string())  # This will print the session string
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
